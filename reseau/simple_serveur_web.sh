4557b7b83702dbfbc1174f449749935cda85fd93
raph <raphaelsouliac@gmail.com>
2025-01-21 16:49:16 +0100

    c'est vraiment utile sql ?

--- reseau/simple_serveur_web.sh
+++ reseau/simple_serveur_web.sh
@@ -21,8 +21,8 @@ sudo apt install mysql-server php libapache2-mod-php php-mysql -y
 
 #Configuration MySQL
 sudo mysql --user=root <<_EOF_
-  UPDATE mysql.user SET Password=PASSWORD('${db_root_password}') WHERE User='root';
-  DELETE FROM mysql.user WHERE User='';
+  UPDATE mysql.user SET Password=PASSWORD(`${db_root_password}`) WHERE User=`root`;
+  DELETE FROM mysql.user WHERE User=``;
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
   DROP DATABASE IF EXISTS test;
   DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
